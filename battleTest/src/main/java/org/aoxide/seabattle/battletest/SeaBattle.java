9f2d79316639fe80e4fdf0f20b4e09e54d7e7c2c
AOxide <AOxide@mail.ru>
2017-03-13 23:56:57 +0400

    bugfix for maven

--- battleTest/src/main/java/org/aoxide/seabattle/battletest/SeaBattle.java
+++ battleTest/src/main/java/org/aoxide/seabattle/battletest/SeaBattle.java
@@ -17,11 +17,11 @@ public class SeaBattle
         DAO dao = (DAO)context.getBean("SpringDAO");
         Game game = new Game(dao, 0L, 1L);
         
-        //createShips(game,0L);
-        //createShips(game,1L);
+        createShips(game,0L);
+        createShips(game,1L);
         
-        //game.shot(0L, 5, 5);
-        //game.shot(1L, 7, 2);
+        game.shot(0L, 5, 5);
+        game.shot(1L, 7, 2);
     }
     
     public static void createShips(Game game, long session_id)
