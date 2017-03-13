4139d884d44e20a704a8a92a55a37caf55292575
AOxide <AOxide@mail.ru>
2017-03-13 23:52:18 +0400

    separate tester

--- battleTest/src/main/java/org/aoxide/seabattle/battletest/SeaBattle.java
+++ battleTest/src/main/java/org/aoxide/seabattle/battletest/SeaBattle.java
@@ -1,7 +1,7 @@
-package org.aoxide.seabattle;
+package org.aoxide.seabattle.battletest;
 
-import org.aoxide.seabattle.dao.*;
-import org.aoxide.seabattle.entities.*;
+import org.aoxide.seabattle.dao.DAO;
+import org.aoxide.seabattle.entities.Game;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,11 +17,11 @@ public class SeaBattle
         DAO dao = (DAO)context.getBean("SpringDAO");
         Game game = new Game(dao, 0L, 1L);
         
-        createShips(game,0L);
-        createShips(game,1L);
+        //createShips(game,0L);
+        //createShips(game,1L);
         
-        game.shot(0L, 5, 5);
-        game.shot(1L, 7, 2);
+        //game.shot(0L, 5, 5);
+        //game.shot(1L, 7, 2);
     }
     
     public static void createShips(Game game, long session_id)
